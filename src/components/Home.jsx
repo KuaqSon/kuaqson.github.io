410afc7d1e6f45ee2d47d5564b9143911eb12162
KuaqSon <qson.ng@gmail.com>
2019-10-01 02:21:18 +0700

    Add greeting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { Box, Text } from "grommet";
+import { Box, Text, Heading } from "grommet";
 
 function Home() {
-  return <Box height="100vh">Home</Box>;
+  return (
+    <Box height="100vh">
+      <Box animation={{type: "zoomOut", duration: 1000, size: "large"}}>
+        <div className="greeting-container">
+          <div className="greeting-sm">Hello there, my name is</div>
+          <div className="greeting-main">Nguyen Quang Son.</div>
+          <div className="greeting-second">I'm a web developer</div>
+        </div>
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
