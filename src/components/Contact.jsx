63eca4bde3e8e812246ed66e43c0440002a33156
KuaqSon <qson.ng@gmail.com>
2019-10-03 02:14:49 +0700

    Updated contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { Box, Text, Heading } from "grommet";
-import SectionTitle from "./SectionTitle";
+import { Box, Text, Anchor } from "grommet";
+import { MailOption } from "grommet-icons";
 
 function Contact() {
   return (
     <Box height={{ min: "80vh" }} id="contact">
       <div className="container">
-        {SectionTitle("Let's connect")}
-        <Box direction="column">
-          <Text>Although I'm not currently looking for freelance opportunities, my inbox is always open. Feel free to connect with me on any of the social platforms below.</Text>
-          <Text>Get in touch</Text>
-          <Text>qson.ng@gmail.com</Text>
+        <Box direction="column" justify="center" pad={{ top: "48px" }}>
+          <Text textAlign="center" size="48px">
+            Let's connect
+          </Text>
+          <Text margin={{ vertical: "medium" }} textAlign="center" size="24px">
+            Although I'm not currently looking for freelance opportunities, my
+            inbox is always open. Feel free to connect with me. I'll try my best
+            to answer your email!
+          </Text>
+
+          <Box pad={{ vertical: "large" }} alignSelf="center">
+            <Anchor
+              icon={<MailOption />}
+              label="Get in touch"
+              href="mailto:qson.ng@gmail.com"
+              size="large"
+            />
+          </Box>
         </Box>
       </div>
     </Box>
