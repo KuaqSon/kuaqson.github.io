69cc21b0758fb8d569c2c9669c394112039ddaa8
KuaqSon <qson.ng@gmail.com>
2019-10-03 02:41:45 +0700

    Updated footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import { Box, Text, Anchor } from "grommet";
 import { MailOption } from "grommet-icons";
+import Fade from "react-reveal/Fade";
 
 function Contact() {
   return (
-    <Box height={{ min: "80vh" }} id="contact">
-      <div className="container">
-        <Box direction="column" justify="center" pad={{ top: "48px" }}>
-          <Text textAlign="center" size="48px">
-            Let's connect
-          </Text>
-          <Text margin={{ vertical: "medium" }} textAlign="center" size="24px">
-            Although I'm not currently looking for freelance opportunities, my
-            inbox is always open. Feel free to connect with me. I'll try my best
-            to answer your email!
-          </Text>
+    <Fade bottom>
+      <Box height={{ min: "80vh" }} id="contact">
+        <div className="container">
+          <Box direction="column" justify="center" pad={{ top: "48px" }}>
+            <Fade bottom>
+              <Box direction="row" justify="center">
+                <Text textAlign="center" size="48px">
+                  Let's connect
+                </Text>
+              </Box>
+            </Fade>
+            <Fade bottom>
+              <Box
+                margin={{ vertical: "medium" }}
+                direction="row"
+                justify="center"
+              >
+                <Text textAlign="center" size="24px">
+                  Although I'm not currently looking for freelance
+                  opportunities, my inbox is always open. Feel free to connect
+                  with me. I'll try my best to answer your email!
+                </Text>
+              </Box>
+            </Fade>
 
-          <Box pad={{ vertical: "large" }} alignSelf="center">
-            <Anchor
-              icon={<MailOption />}
-              label="Get in touch"
-              href="mailto:qson.ng@gmail.com"
-              size="large"
-            />
+            <Fade bottom>
+              <Box pad={{ vertical: "large" }} justify="center" direction="row">
+                <Anchor
+                  icon={<MailOption />}
+                  label="Get in touch"
+                  href="mailto:qson.ng@gmail.com"
+                  size="large"
+                />
+              </Box>
+            </Fade>
           </Box>
-        </Box>
-      </div>
-    </Box>
+        </div>
+      </Box>
+    </Fade>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { Box, Text } from "grommet";
-import { Article } from "grommet-icons";
+import { Box, Text, Anchor } from "grommet";
+import { Article, Facebook, Github } from "grommet-icons";
 
 function Footer() {
   return (
-    <Box
-      direction="row"
-      justify="center"
-      align="center"
-      gap="medium"
-      pad="medium"
-      background={{ color: "brand" }}
-    >
-      <Text>2019</Text>
-      <Article />
-      <Text>Made by Nguyen Quang Son</Text>
+    <Box background={{ color: "brand" }} pad="small">
+      <Box
+        direction="row"
+        justify="center"
+        align="center"
+        gap="medium"
+      >
+        <Anchor icon={<Facebook />} href="https://www.facebook.com/quangson.nguyen.399" target="_blank" color="#fff" />
+        <Anchor icon={<Github />} href="https://github.com/KuaqSon" target="_blank" color="#fff" />
+      </Box>
+      <Box
+        direction="row"
+        justify="center"
+        align="center"
+        gap="medium"
+        pad="small"
+      >
+        <Text>2019</Text>
+        <Article />
+        <Text>Made by Nguyen Quang Son</Text>
+      </Box>
     </Box>
   );
 }
