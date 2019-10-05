f5b419b65fbef18b20f4aa1890ddfde3c3099b79
KuaqSon <qson.ng@gmail.com>
2019-10-05 15:20:07 +0700

    Add background animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,10 @@ import { Box, Text, Heading } from "grommet";
 
 function Home() {
   return (
-    <Box height={{min: "100vh"}}>
+    <Box height={{min: "100vh"}} justify="center">
       <Box animation={{type: "zoomOut", duration: 1000, size: "large"}}>
         <div className="container greeting-container">
-          <div className="greeting-sm">Hello there, my name is</div>
+          <div className="greeting-sm">Hello, my name is</div>
           <div className="greeting-main">Nguyen Quang Son.</div>
           <div className="greeting-second">I'm a web developer</div>
         </div>
