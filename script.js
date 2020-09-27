f6ffac2a86d245ddea18d070b0d8e023ac73a95c
kuaqson <qson.ng@gmail.com>
2020-09-27 22:47:51 +0700

    :information_desk_person:

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-;(function () {
-    let card  = document.getElementsByClassName('card')[0],
-        moved = 0,
-        interval;
-
-    if (!card) return;
-    
-    card.addEventListener('click', function (event) {
-        clearInterval(interval);
-        card.style.transform = '';
-        
-        // Do not flip the card if the user is trying to
-        // tap a link.
-        if (event.target.nodeName === 'A') {
-            return;
-        }
-        
-        let cName   = card.getAttribute('data-toggle-class');
-        let toggled = card.classList.contains(cName);
-        card.classList[toggled ? 'remove' : 'add'](cName);
+(function () {
+  function autoType(elementClass, typingSpeed) {
+    var thhis = $(elementClass);
+    thhis.css({
+      position: "relative",
+      display: "inline-block",
     });
-    
-    interval = setInterval(function () {
-        moved = moved ? 0 : 10;
-        card.style.transform = 'translateY(' + moved + 'px)';
-    }, 1500);
+    thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+    thhis = thhis.find(".text-js");
+    var text = thhis.text().trim().split("");
+    var amntOfChars = text.length;
+    var newString = "";
+    thhis.text("|");
+    setTimeout(function () {
+      thhis.css("opacity", 1);
+      thhis.prev().removeAttr("style");
+      thhis.text("");
+      for (var i = 0; i < amntOfChars; i++) {
+        (function (i, char) {
+          setTimeout(function () {
+            newString += char;
+            thhis.text(newString);
+          }, i * typingSpeed);
+        })(i + 1, text[i]);
+      }
+    }, 1000);
+  }
+
+  function initTheme() {
+    var hour = new Date().getHours();
+    if (hour >= 18 || hour <= 6) {
+      $("body").removeClass("light");
+    } else {
+      $("body").addClass("light");
+    }
+  }
+
+  $(document).ready(function () {
+    initTheme();
+
+    // Now to start autoTyping just call the autoType function with the
+    // class of outer div
+    // The second paramter is the speed between each letter is typed.
+    autoType(".type-js", 100);
+  });
 })();
